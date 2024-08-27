35f6e1617530d9b207f63543e1fdb4449c1de397
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-08-27 14:33:13 +0300

    feat: token validation

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -10,11 +10,14 @@ import {
 } from "../controllers/contactsControllers.js";
 import validateBody from "../helpers/validateBody.js";
 import {createContactSchema, updateContactSchema} from "../schemas/contactsSchemas.js";
+import authenticate from "../middlewares/authenticate.js";
 
 const createMiddleware = validateBody(createContactSchema);
 const updateMiddleware = validateBody(updateContactSchema);
 
 const contactsRouter = express.Router();
+
+contactsRouter.use(authenticate);
 
 contactsRouter.get("/", getAllContacts);
 
