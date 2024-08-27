386179a15f1658e89517da01619263a6be11e2a9
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-08-27 11:58:01 +0300

    feat: added routes for registartion and login

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,14 @@
 import express from "express";
+import {login, register} from "../controllers/authControllers.js";
+import validateBody from "../helpers/validateBody.js";
+import {userSignInSchema, userSignupSchema} from "../schemas/userSchemas.js";
+
+const registerMiddleware = validateBody(userSignupSchema);
+const loginMiddleware = validateBody(userSignInSchema);
 
 const authRouter = express.Router();
+
+authRouter.post("/register", registerMiddleware, register);
+authRouter.post("/login", loginMiddleware, login);
 
 export default authRouter;
