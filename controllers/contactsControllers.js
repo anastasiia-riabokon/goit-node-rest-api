6f990e45d2aa3c1bce40d62eb5e3f8094dfe53fc
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-08-27 19:31:06 +0300

    feat: added filter by favorite

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,9 +4,15 @@ import * as contactsService from "../services/contactsServices.js";
 
 export const getAllContacts = ctrlWrapper(async (req, res) => {
   const {_id: owner} = req.user;
-  const {page = 1, limit = 10} = req.query;
+  const {page = 1, limit = 10, favorite} = req.query;
   const skip = (page - 1) * limit;
-  const result = await contactsService.listContacts({owner}, {skip, limit});
+
+  const filter = {owner};
+  if (favorite !== undefined) {
+    filter.favorite = favorite === "true";
+  }
+
+  const result = await contactsService.listContacts(filter, {skip, limit});
 
   res.json(result);
 });
